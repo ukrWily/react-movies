6b9afdda5fab87612445f1d3a12350fe33e2a04e
ukrWily <ukrwily@gmail.com>
2025-01-24 13:48:09 +0200

    Added movies, movie

---
+++ src/components/Movie.jsx
@@ -0,0 +1,22 @@
+export function Movie(props) {
+  const {
+    Title: title,
+    Year: year,
+    imdbID: id,
+    Type: type,
+    Poster: poster,
+  } = props;
+  return (
+    <div id={id} className="card movie">
+      <div className="card-image waves-effect waves-block waves-light">
+        <img className="activator" src={poster} alt={title} />
+      </div>
+      <div className="card-content">
+        <span className="card-title activator">{title}</span>
+        <p>
+          {year} <span className="right">{type}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,3 +1,28 @@
-export function Main() {
-  return <main className="container content"></main>;
+import React from "react";
+import { Movies } from "../components/Movies";
+
+export class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch("https://www.omdbapi.com/?apikey=4a3b711b&s=matrix")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <main className="container content">
+        {movies.length ? (
+          <Movies movies={movies} />
+        ) : (
+          <h4>What can i find for you?</h4>
+        )}
+      </main>
+    );
+  }
 }
