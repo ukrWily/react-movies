acd77ef2e5394f3a6710a41f080f54d8699a6379
ukrWily <ukrwily@gmail.com>
2025-01-26 13:26:30 +0200

    Added filter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,40 +9,80 @@ export class Search extends Component {
 
   state = {
     search: "",
+    type: "all",
   };
 
   movieSearch = () => {
     if (this.state.search.trim() === "") {
       return;
     }
-    this.props.search(this.state.search);
-    this.setState({ search: "" });
+    this.props.search(this.state.search, this.state.type);
+    // this.setState({ search: "" });
   };
 
   handleKey(e) {
     if (e.key === "Enter") {
-      this.movieSearch();
+      this.movieSearch(this.state.search, this.state.type);
     }
   }
 
+  handleFilter = (e) => {
+    this.setState(
+      () => ({ type: e.target.value }),
+      () => this.props.search(this.state.search, this.state.type)
+    );
+  };
+
   render() {
     return (
-      <div class="row search">
-        <div class="input-field col s6">
-          <input
-            placeholder="Search movie"
-            id="input_text"
-            type="search"
-            value={this.state.search}
-            onChange={(e) => this.setState({ search: e.target.value })}
-            onKeyDown={(e) => this.handleKey(e)}
-          />
-          {this.state.search.trim() !== "" && (
-            <button className="search-button" onClick={this.movieSearch}>
-              Search
-            </button>
-          )}
+      <div className="search-wrapper">
+        <div class="row search">
+          <div class="input-field col s6">
+            <input
+              placeholder="Search movie"
+              id="input_text"
+              type="search"
+              value={this.state.search}
+              onChange={(e) => this.setState({ search: e.target.value })}
+              onKeyDown={(e) => this.handleKey(e)}
+            />
+            {this.state.search.trim() !== "" && (
+              <button className="search-button" onClick={this.movieSearch}>
+                Search
+              </button>
+            )}
+          </div>
         </div>
+
+        <form className="typeMovies" onChange={this.handleFilter}>
+          <label>
+            <input
+              name="type"
+              type="radio"
+              value="all"
+              checked={this.state.type === "all"}
+            />
+            <span>All</span>
+          </label>
+          <label>
+            <input
+              name="type"
+              type="radio"
+              value="movie"
+              checked={this.state.type === "movie"}
+            />
+            <span>Movie</span>
+          </label>
+          <label>
+            <input
+              name="type"
+              type="radio"
+              value="series"
+              checked={this.state.type === "series"}
+            />
+            <span>Series</span>
+          </label>
+        </form>
       </div>
     );
   }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Movies } from "../components/Movies";
 import { Preloader } from "../components/Preloader";
 import { Search } from "../components/Search";
-
 export class Main extends React.Component {
   state = {
     movies: [],
   };
 
-  handleSearch = (search = "matrix") => {
-    fetch(`https://www.omdbapi.com/?apikey=4a3b711b&s=${search}`)
+  handleSearch = (search, type = "all") => {
+    fetch(
+      `https://www.omdbapi.com/?apikey=4a3b711b&s=${search}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
+    )
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search }));
   };
@@ -23,8 +26,8 @@ export class Main extends React.Component {
 
     return (
       <main className="content">
-        <Search search={this.handleSearch} />
-        {!movies && <div className="center">No movies found</div>}
+        <Search search={this.handleSearch} handleType={this.handleType} />
+        {!movies && <div className="center">Movies not found</div>}
         {movies && movies.length > 0 ? (
           <Movies movies={movies} />
         ) : (
